fa6ef762c54a67e799f083bb6b7da1d2be92ac36
mikerr1 <michaelraymond2@gmail.com>
2024-08-26 21:37:24 +0700

    first commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='zenify',
+    version='0.0.1',
+    description='data scraping framework',
+    url='git@github.com:mikerr1/zenify.git',
+    author='Michael Raymond',
+    author_email='michaelraymond2@gmail.com',
+    license='MIT',
+    packages=['zenify'],
+    zip_safe=False
+)
